31006a7722702db3f8532a63c0d07f3f6257c93a
sjy457346652 <roart_duck@163.com>
2017-10-31 00:09:52 +0800

    commit base structure

---
+++ SwiftStudy/SwiftStudy/Entry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by yang song on 30/10/2017.
+//  Copyright © 2017 yangsong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
