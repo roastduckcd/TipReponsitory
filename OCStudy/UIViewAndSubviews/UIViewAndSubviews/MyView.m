2d0a00838b8e92b00b5cf76f3abc60d0d747f728
sjy457346652 <roart_duck@163.com>
2017-11-08 14:56:01 +0800

    提交UIView内部执行顺序修改

--- OCStudy/UIViewAndSubviews/UIViewAndSubviews/MyView.m
+++ OCStudy/UIViewAndSubviews/UIViewAndSubviews/MyView.m
@@ -10,12 +10,49 @@
 
 @implementation MyView
 
-/*
+#pragma mark - 绘制   rendering
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    NSLog(@"draw rect");
 }
-*/
+- (void)setNeedsDisplay {
+        NSLog(@"setNeedsDisplay");
+}
+- (void)setNeedsDisplayInRect:(CGRect)rect {
+    NSLog(@"setNeedsDisplayInRect");
+}
+
+#pragma mark - 层级   hirarchy
+- (void)layoutSubviews {
+    NSLog(@"layoutSubviews");
+}
+- (void)setNeedsLayout {
+    NSLog(@"setNeedsLayout");
+}
+- (void)layoutIfNeeded {
+    NSLog(@"layoutIfNeeded");
+}
+
+#pragma mark - 几何  geometry
+/**
+ calls sizeThatFits: with current view bounds and changes bounds size.
+ */
+- (void)sizeToFit {
+    NSLog(@"sizeToFit");
+}
+/**
+return 'best' size to fit given size. does not actually resize view. Default is return existing view size
+
+ @param size 给定大小
+ @return 返回适应大小
+ */
+- (CGSize)sizeThatFits:(CGSize)size {
+    NSLog(@"sizeThatFits");
+    return [super sizeThatFits:size];
+}
+
+
 
 @end
--- OCStudy/UIViewAndSubviews/UIViewAndSubviews/ViewController.m
+++ OCStudy/UIViewAndSubviews/UIViewAndSubviews/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "MyView.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) MyView *myView;
 
 @end
 
@@ -16,13 +18,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.myView = [[MyView alloc] initWithFrame:CGRectMake(10, 10, self.view.frame.size.width - 10 * 2, self.view.frame.size.height - 10 * 2)];
+    [self.view addSubview:self.myView];
+
+
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 300, 30)];
+    label.text = @"test nooonononono";
+    [self.myView addSubview:label];
+
+
+
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 50, 50)];
+    [button setTitle:@"按钮" forState: UIControlStateNormal];
+    [button addTarget:self action:@selector(clicked) forControlEvents:UIControlEventTouchUpInside];
+    [self.myView addSubview:button];
+
+//    myView.frame = CGRectMake(30, 30, 100, 100);
+
 }
 
+- (void)clicked {
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 100, 30)];
+//    label.text = @"test";
+//    [self.myView addSubview: label];
+
+//    self.myView.frame = CGRectMake(10, 10, 10, 10);
+//    self.myView.bounds = CGRectMake(0, 0, 10, 10);
+
+//    [self.myView setNeedsLayout];
+//    [self.myView layoutIfNeeded];
+//    [self.myView setNeedsDisplay];
 }
 
 
